07d6b55a0cda1d386d57ff8eec0121563b09fc63
Giorgi Koguashvili <kogo27@gmail.com>
2017-12-26 19:09:10 +0400

    added RabbitMQ

---
+++ Sagas.Service.B/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Sagas.Service.B
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+			var factory = new ConnectionFactory()
+			{
+				HostName = "message-bus",
+				UserName = "user",
+				Password = "1234"
+			};
+
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(queue: "hello",
+									 durable: false,
+									 exclusive: false,
+									 autoDelete: false,
+									 arguments: null);
+
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += (model, ea) =>
+				{
+					var body = ea.Body;
+					var message = Encoding.UTF8.GetString(body);
+					Console.WriteLine(" [x] Received {0}", message);
+				};
+				channel.BasicConsume(queue: "hello",
+									 autoAck: true,
+									 consumer: consumer);
+
+				Console.WriteLine(" Press [enter] to exit.");
+				Console.ReadLine();
+			}
+		}
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
